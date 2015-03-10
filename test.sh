f012d79c59d248acc287e9596dd06d27e4762ecd
Sam-Burns <sam.burns.uk@gmail.com>
2015-03-10 23:07:37 +0000

    Removing support for < php5.4 from test script

--- test.sh
+++ test.sh
@@ -20,13 +20,7 @@ vendor/bin/behat --config tests/behat/behat.yml --suite all_features;
 echo -e '\n\n#######################';
 echo '### BEHAT WEBSERVER ###';
 echo -e '#######################\n\n';
-PHP_VERSION_HAS_SERVER=`php -r 'echo version_compare(PHP_VERSION, "5.4.0") >= 0;'`;
-if [ $PHP_VERSION_HAS_SERVER ] ;
-    then
-        PROCESS_ID=`php -S localhost:8080 ./src-dev/src/index.php > /dev/null & echo $!`;
-        vendor/bin/behat --config tests/behat/behat.yml --suite webserver_features;
-        kill -9 $PROCESS_ID;
-    else
-        echo -e 'ONLY RUNS WITH PHP5.4+\n\n';
-fi ;
+PROCESS_ID=`php -S localhost:8080 ./src-dev/src/index.php > /dev/null & echo $!`;
+vendor/bin/behat --config tests/behat/behat.yml --suite webserver_features;
+kill -9 $PROCESS_ID;
 echo -e '\n';
