a8e8e5e5b2c2abb7d4904231b49958aae0929713
Sam-Burns <sam.burns.uk@gmail.com>
2015-05-06 16:19:50 +0100

    Updating travis config approach

--- tests/test-travis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-clear;
-./vendor/bin/phpunit --config tests/phpunit/phpunit-travis.xml;
-./vendor/bin/phpspec run --config tests/phpspec/phpspec-travis.yml --no-interaction;
-./vendor/bin/behat --config tests/behat/behat.yml --suite inline;
