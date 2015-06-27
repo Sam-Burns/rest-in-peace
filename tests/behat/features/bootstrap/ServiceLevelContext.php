a17983b8d94bac4047ab00f92396ea52e663a898
Sam-Burns <sam.burns.uk@gmail.com>
2015-06-28 00:06:21 +0100

    Fixing Behat integration

--- tests/behat/features/bootstrap/ServiceLevelContext.php
+++ tests/behat/features/bootstrap/ServiceLevelContext.php
@@ -4,6 +4,6 @@ use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Context\Context;
 
-class InlineContext implements Context, SnippetAcceptingContext
+class ServiceLevelContext implements Context, SnippetAcceptingContext
 {
 }
--- tests/behat/features/bootstrap/WebserverContext.php
+++ tests/behat/features/bootstrap/WebserverContext.php
@@ -4,6 +4,6 @@ use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Context\Context;
 
-class ServiceLevelContext implements Context, SnippetAcceptingContext
+class WebserverContext implements Context, SnippetAcceptingContext
 {
 }
