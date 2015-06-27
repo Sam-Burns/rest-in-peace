486858c857d5cc596f6a0cfbe400c7eca5df4a7a
Sam-Burns <sam.burns.uk@gmail.com>
2015-06-27 23:30:28 +0100

    Sorting out project scaffolding

---
+++ tests/behat/features/bootstrap/WebserverContext.php
@@ -0,0 +1,9 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Context\Context;
+
+class ServiceLevelContext implements Context, SnippetAcceptingContext
+{
+}
