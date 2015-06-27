80a397d57719bef25269fe08f200489f8970cbe6
Sam-Burns <sam.burns.uk@gmail.com>
2015-06-28 00:45:00 +0100

    Adding request object; reference to front controller

--- tests/behat/features/bootstrap/ServiceLevelContext.php
+++ tests/behat/features/bootstrap/ServiceLevelContext.php
@@ -3,15 +3,31 @@
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Context\Context;
+use Zend\Diactoros\Request;
+use DirtyNeedle\DiContainer;
+//use RestInPeace\FrontController;
 
 class ServiceLevelContext implements Context, SnippetAcceptingContext
 {
+    public function __construct()
+    {
+        require_once __DIR__ . '/../../../../src/bootstrap.php';
+    }
+
     /**
      * @When I send a GET request to :path
      */
     public function iSendAGetRequestTo($path)
     {
-        throw new PendingException();
+        $request = new Request($path, 'GET');
+        $frontController = $this->getFrontController();
+        $response = $frontController->getResponseForRequest($request);
+    }
+
+    private function getFrontController()
+    {
+        $diContainer = DiContainer::getInstance();
+        return $diContainer->get('restinpeace.frontcontroller');
     }
 
     /**
