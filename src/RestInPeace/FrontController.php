8b07d73778f6564bf10d0d4a7196c922964fd5ab
Sam-Burns <sam.burns.uk@gmail.com>
2015-06-28 01:23:52 +0100

    Adding request and response parameters to front controller

--- src/RestInPeace/FrontController.php
+++ src/RestInPeace/FrontController.php
@@ -1,7 +1,17 @@
 <?php
-
 namespace RestInPeace;
+
+use Psr\Http\Message\RequestInterface as PsrRequest;
+use Psr\Http\Message\ResponseInterface as PsrResponse;
 
 class FrontController
 {
+    /**
+     * @param PsrRequest $request
+     * @return PsrResponse
+     */
+    public function getResponseForRequest(PsrRequest $request)
+    {
+        // TODO: write logic here
+    }
 }
--- tests/phpspec/RestInPeace/Spec/RestInPeace/FrontControllerSpec.php
+++ tests/phpspec/RestInPeace/Spec/RestInPeace/FrontControllerSpec.php
@@ -1,14 +1,19 @@
 <?php
-
 namespace RestInPeace\Spec\RestInPeace;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Psr\Http\Message\RequestInterface;
 
 class FrontControllerSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('RestInPeace\FrontController');
+    }
+
+    function it_can_return_http_responses(RequestInterface $request)
+    {
+        $response = $this->getResponseForRequest($request);
     }
 }
