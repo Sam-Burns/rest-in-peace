d5fef30deeeabb1f16cf4565d7346b73834e11a0
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-03 00:49:03 +0100

    Setup for sample app

--- tests/test-dev.sh
+++ tests/test-dev.sh
@@ -3,4 +3,8 @@
 clear;
 ./vendor/bin/phpunit --config tests/phpunit/phpunit.xml;
 ./vendor/bin/phpspec run --config tests/phpspec/phpspec.yml;
-./vendor/bin/behat --config tests/behat/behat.yml;
+./vendor/bin/behat --config tests/behat/behat.yml --suite servicelevel;
+
+php -S localhost:8081 sample-app/public/index.php &> /dev/null &
+WEBSERVER_PROCESS_ID=$!;
+kill -9 $WEBSERVER_PROCESS_ID;
