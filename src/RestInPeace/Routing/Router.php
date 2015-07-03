b40248a38fb82b55a80476674998fd365e4378ad
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-03 02:17:49 +0100

    Adding placeholder for method to select route

--- src/RestInPeace/Routing/Router.php
+++ src/RestInPeace/Routing/Router.php
@@ -1,6 +1,15 @@
 <?php
 namespace RestInPeace\Routing;
 
+use Psr\Http\Message\RequestInterface as PsrRequest;
+
 class Router
 {
+    /**
+     * @param PsrRequest $request
+     */
+    public function getRouteForRequest(PsrRequest $request)
+    {
+        // TODO: write logic here
+    }
 }
--- tests/phpspec/RestInPeace/Spec/RestInPeace/Routing/RouterSpec.php
+++ tests/phpspec/RestInPeace/Spec/RestInPeace/Routing/RouterSpec.php
@@ -3,11 +3,17 @@ namespace RestInPeace\Spec\RestInPeace\Routing;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Psr\Http\Message\RequestInterface as PsrRequest;
 
 class RouterSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('RestInPeace\Routing\Router');
+    }
+
+    function it_can_get_a_route_for_a_request(PsrRequest $request)
+    {
+        $this->getRouteForRequest($request);
     }
 }
