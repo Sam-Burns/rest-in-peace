e729ee493a9e334be5a1692bd4c5b1ce64bdf37b
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-04 16:19:54 +0100

    Adding functionality to Route object implied by PHPUnit integration tests

--- src/RestInPeace/Routing/Route.php
+++ src/RestInPeace/Routing/Route.php
@@ -3,4 +3,35 @@ namespace RestInPeace\Routing;
 
 class Route
 {
+    /** @var string */
+    private $name;
+
+    /** @var array */
+    private $config;
+
+    /**
+     * @param string $name
+     * @param array  $config
+     */
+    public function __construct($name, $config)
+    {
+        $this->name = $name;
+        $this->config = $config;
+    }
+
+    /**
+     * @return string
+     */
+    public function getControllerServiceId()
+    {
+        return $this->config['controller-service-id'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getActionMethodName()
+    {
+        return $this->config['action-method-name'];
+    }
 }
--- tests/phpspec/RestInPeace/Spec/RestInPeace/Routing/RouteSpec.php
+++ tests/phpspec/RestInPeace/Spec/RestInPeace/Routing/RouteSpec.php
@@ -6,8 +6,31 @@ use Prophecy\Argument;
 
 class RouteSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedWith(
+            'index',
+            [
+                'route'                 => '/',
+                'method'                => 'GET',
+                'controller-service-id' => 'controllers.index',
+                'action-method-name'    => 'indexAction',
+            ]
+        );
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('RestInPeace\Routing\Route');
+    }
+
+    function it_can_provide_a_controller_service_id()
+    {
+        $this->getControllerServiceId()->shouldBe('controllers.index');
+    }
+
+    function it_can_provide_an_action_method_name()
+    {
+        $this->getActionMethodName()->shouldBe('indexAction');
     }
 }
