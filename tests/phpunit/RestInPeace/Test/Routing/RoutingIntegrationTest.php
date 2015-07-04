c879b0684fc2a994cf7497c3bae3576faed2dd72
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-05 00:53:42 +0100

    Fleshing out skipped PHPUnit test for routing

--- tests/phpunit/RestInPeace/Test/Routing/RoutingIntegrationTest.php
+++ tests/phpunit/RestInPeace/Test/Routing/RoutingIntegrationTest.php
@@ -4,6 +4,7 @@ namespace RestInPeace\Test\Routing;
 use RestInPeace\Routing\Router;
 use PHPUnit_Framework_TestCase as TestCase;
 use Zend\Diactoros\Request;
+use ConfigTree\Builder\ConfigTreeBuilder;
 
 class RoutingIntegrationTest extends TestCase
 {
@@ -13,15 +14,26 @@ class RoutingIntegrationTest extends TestCase
 
         // ARRANGE
         $request = new Request('http://hostname.com/', 'GET');
-
-        $router = new Router();
-        $router->addConfig(__DIR__ . '/fixtures/sample_routing_file.php');
+        $router = $this->getRouter();
 
         // ACT
         $route = $router->getRouteForRequest($request);
 
         // ASSERT
+        $this->assertInstanceOf('\RestInPeace\Routing\Route', $route);
         $this->assertEquals('controllers.index', $route->getControllerServiceId());
         $this->assertEquals('indexAction',       $route->getActionMethodName());
+    }
+
+    /**
+     * @return Router
+     */
+    private function getRouter()
+    {
+        $configBuilder = new ConfigTreeBuilder();
+        $configBuilder->addSettingsFromPaths([__DIR__ . '/fixtures/sample_routing_file.php']);
+        $wholeConfig = $configBuilder->buildConfigTreeAndReset();
+        $routingConfig = $wholeConfig->getSubtreeFromPath('rest-in-peace/routing');
+        return new Router($routingConfig);
     }
 }
